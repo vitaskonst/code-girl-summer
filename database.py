66c36fd97778de245648c428bc9921b8b9163cf8
RKhamitov <one_RKhamitov@fortebank.com>
2023-08-03 12:08:56 +0600

    added database integration

---
+++ database.py
@@ -0,0 +1,38 @@
+# pip install databases sqlalchemy asyncpg psycopg2-binary
+from databases import Database
+from sqlalchemy import (
+    MetaData,
+    create_engine,
+    Table,
+    Column,
+    Integer,
+    Identity,
+    String,
+    Text,
+    DateTime,
+    func
+)
+
+#  insert 
+
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
+# DATABASE_URL = "postgresql://codegirl:password@localhost:5432/database1"
+
+database = Database(DATABASE_URL)
+metadata = MetaData()
+
+posts = Table(
+    "posts",
+    metadata,
+    Column("id", Integer, Identity(), primary_key=True),
+    Column("title", String),
+    Column("contet", Text),
+    Column("author", String),
+    Column("location", String),
+    Column("created_at", DateTime, server_default_func=func.now()),
+    Column("updated_at", DateTime, onupdate=func.now())
+)
+
+engine = create_engine(DATABASE_URL)
+
+metadata.create_all(engine)
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from posts.router import router as posts_router 
+from database import database
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 app.include_router(posts_router, prefix="/posts")
 
