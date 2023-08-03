599a632dcb95b2fdc7dc1934a99021b1d44f0fb4
RKhamitov <one_RKhamitov@fortebank.com>
2023-08-03 16:53:03 +0600

    added users table

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ from sqlalchemy import (
     func
 )
 
-#  insert 
+#  insert
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 # DATABASE_URL = "postgresql://codegirl:password@localhost:5432/database1"
@@ -31,6 +31,24 @@ posts = Table(
     Column("location", String),
     Column("created_at", DateTime, server_default=func.now()),
     Column("updated_at", DateTime, onupdate=func.now())
+)
+
+# id - serial
+# username - string
+# password - string
+# first_name - string
+# last_name - string
+# created_at - DateTime
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, Identity(), primary_key=True),
+    Column("username", String, unique=True),
+    Column("password", String),
+    Column("first_name", String),
+    Column("last_name", String),
+    Column("created_at", DateTime)
 )
 
 engine = create_engine(DATABASE_URL)
