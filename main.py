12a5545b4ebffacdb2cc6b6486f7338b786c132c
Vitaliy Konstantinov <mr.vitaliy.konstantinov@mail.ru>
2023-08-04 15:58:26 +0600

    added image feature

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
+
+from fastapi import FastAPI, UploadFile
+from starlette.middleware.cors import CORSMiddleware
 from posts.router import router as posts_router 
 from users.router import router as users_router
 from database import database
+from typing import List
+
+import cloudinary
+from cloudinary.uploader import upload
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup():
@@ -12,6 +28,18 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+
+@app.post('/images')
+def upload_files(files: List[UploadFile]):
+    urls = []
+    for file in files:
+        result = upload(file.file.read())
+        urls.append(result['secure_url'])
+
+    return {
+        'urls': urls
+    }
 
 
 app.include_router(posts_router, prefix="/posts", tags=["Posts"])
