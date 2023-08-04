d35c990500f7c77f4e347666d8eec595f0605be4
Vitaliy Konstantinov <mr.vitaliy.konstantinov@mail.ru>
2023-08-04 16:13:52 +0600

    resolve merge conflicts

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 from starlette.middleware.cors import CORSMiddleware
 from posts.router import router as posts_router 
 from users.router import router as users_router
 from database import database
+import requests
 from typing import List
 
 import cloudinary
@@ -41,6 +42,36 @@ def upload_files(files: List[UploadFile]):
         'urls': urls
     }
 
+
+@app.post('/images')
+def upload_files(files: List[UploadFile]):
+    urls = []
+    for file in files:
+        result = upload(file.file.read())
+        urls.append(result['secure_url'])
+
+    return {
+        'urls': urls
+    }
+
+
+def get_dog_image_url():
+    url = "https://dog.ceo/api/breeds/image/random"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["message"]
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to retrieve the dog image")
+
+@app.get("/dogs/image")
+async def get_dog_image():
+    try:
+        image_url = get_dog_image_url()
+        return {"image_url": image_url}
+    except HTTPException as e:
+        raise e
 
 app.include_router(posts_router, prefix="/posts", tags=["Posts"])
 app.include_router(users_router, prefix="/users", tags=["Users"])
