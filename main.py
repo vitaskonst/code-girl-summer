2e4e7f007039ff80befa391ed87d39e7cdd7f482
RKhamitov <one_RKhamitov@fortebank.com>
2023-08-04 15:55:02 +0600

    dogs api

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from posts.router import router as posts_router 
 from users.router import router as users_router
 from database import database
+import requests
 
 app = FastAPI()
 
@@ -13,6 +14,24 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+
+def get_dog_image_url():
+    url = "https://dog.ceo/api/breeds/image/random"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["message"]
+    else:
+        raise HTTPException(status_code=response.status_code, detail="Failed to retrieve the dog image")
+
+@app.get("/dogs/image")
+async def get_dog_image():
+    try:
+        image_url = get_dog_image_url()
+        return {"image_url": image_url}
+    except HTTPException as e:
+        raise e
 
 app.include_router(posts_router, prefix="/posts", tags=["Posts"])
 app.include_router(users_router, prefix="/users", tags=["Users"])
